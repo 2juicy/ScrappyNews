30e1dd2c7228f5c7ca139cfb34a961697ccc32c0
JohnnyC <j0hnnywchiang@gmail.com>
2018-07-08 16:23:21 -0700

    scrapper moved to routing

--- server.js
+++ server.js
@@ -3,12 +3,6 @@ const bodyParser = require("body-parser");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
-// Our scraping tools
-// Axios is a promised-based http library, similar to jQuery's Ajax method
-// It works on the client and on the server
-const axios = require("axios");
-const cheerio = require("cheerio");
-
 // Require all models
 const db = require("./models");
 

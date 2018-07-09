e73b49ef5a0d90e7f76b98dd29e97e9ce8fc8114
JohnnyC <j0hnnywchiang@gmail.com>
2018-07-09 11:09:41 -0700

    fixed DB added more information to store

--- models/Article.js
+++ models/Article.js
@@ -6,16 +6,23 @@ var Schema = mongoose.Schema;
 // Using the Schema constructor, create a new UserSchema object
 // This is similar to a Sequelize model
 var ArticleSchema = new Schema({
-  // `title` is required and of type String
   title: {
     type: String,
     required: true
   },
-  // `link` is required and of type String
+  info: {
+    type: String,
+    required: true
+  },
   link: {
     type: String,
     required: true
   },
+  img: {
+    type: String,
+    required: true
+  },
+
   // `note` is an object that stores a Note id
   // The ref property links the ObjectId to the Note model
   // This allows us to populate the Article with an associated Note

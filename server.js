6dcc29f3dfa3499ceaa866a34ffcdc037edcdd56
JohnnyC <j0hnnywchiang@gmail.com>
2018-07-09 20:21:46 -0700

    trying to redeploy

--- server.js
+++ server.js
@@ -17,9 +17,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
 
-// Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/mongoHeadlines");
-
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 

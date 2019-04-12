f04366307741eca346fdbeb8056c3bd894229443
JohnnyC <j0hnnywchiang@gmail.com>
2019-04-12 06:24:28 -0700

    added source and icon

--- server.js
+++ server.js
@@ -18,7 +18,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
@@ -30,10 +31,12 @@ const exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them. 
+// Import routes and give the server access to them.
 // Routes
 // =============================================================
 require("./routes/controller.js")(app);
 
 // Start the server
-app.listen(PORT, () => console.log(`\nListening on port http://localhost:${PORT}`));
+app.listen(PORT, () =>
+  console.log(`\nListening on port http://localhost:${PORT}`)
+);
